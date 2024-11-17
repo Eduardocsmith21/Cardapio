ecaf7fc6f898c9d25801dfaf4b2e50b60185c2e8
Eduardo C. Smith <eduardocsmith021@gmail.com>
2024-11-17 14:43:37 -0400

    Atualização de update de carrinho, adicionando items e fazendo soma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cardapio = document.getElementById("cardapio");
 const carrinhoBtn = document.getElementById("carrinho-btn");
 const carrinhoModal = document.getElementById("carrinho-modal");
-const carrinhoItemsContainer = document.getElementById("carrinho-itens");
+const carrinhoItemsContainer = document.getElementById("carrinho-items");
 const carrinhoTotal = document.getElementById("carrinho-total");
 const checkBtn = document.getElementById("check-btn");
 const fecharBtn = document.getElementById("fechar-modal-btn");
@@ -9,9 +9,12 @@ const carrinhoCont = document.getElementById("carrinho-cont");
 const endereco = document.getElementById("endereco");
 const enderecoErro = document.getElementById("endereco-erro");
 
+let carrinho = [];
+
 //Abrir o modal do carrinho
 carrinhoBtn.addEventListener("click", function(){
-    carrinhoModal.style.display = "flex"
+    updateCarrinhoModal();
+    carrinhoModal.style.display = "flex";
 });
 
 //fechar modal quando houver clik fora do modal
@@ -41,6 +44,57 @@ cardapio.addEventListener("click", function(event){
 });
 
 //função para adcionar no carrinho 
-function addCarrinho(nome, preco){
-    alert("O item é " + nome)
+function addCarrinho(nome, preco, quandidade){
+    const itemDuplicado = carrinho.find(item => item.nome == nome)
+
+    if(itemDuplicado){
+        //se o item já existir na lista ele vai apenas mudar a quandidade do item ao invés de adicionar um novo item
+        itemDuplicado.quantidade += 1;
+        return;
+    }
+    carrinho.push({
+        nome,
+        preco,
+        quantidade: 1,
+    })
 }
+
+//atualizar carrinho
+function updateCarrinhoModal(){
+    carrinhoItemsContainer.innerHTML = "";
+    let total = 0;
+
+    carrinho.forEach(item => {
+        const itemElemento = document.createElement("div");
+        itemElemento.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        itemElemento.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-medium">${item.nome}</p>
+                    <p>Qtd: ${item.quantidade}</p>
+                    <p class="font-medium mt-2"> R$ ${item.preco.toFixed(2)}</p>
+                </div>
+
+                <div>
+                    <button class="text-red-600">
+                        remover
+                    </button>
+                </div>
+            </div>
+        `;
+        
+        total += item.preco * item.quandidade
+        carrinhoItemsContainer.appendChild(itemElemento);
+    });
+
+    carrinhoTotal.textContent = total.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    carrinhoCont.innerHTML = carrinho.length;
+
+} 
+
+//função para remover um item do carrinho 
