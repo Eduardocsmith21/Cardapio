3ece29cf965c684def33c5bf1dfa06f193d4204b
Eduardo C. Smith <eduardocsmith021@gmail.com>
2024-11-17 11:37:23 -0400

    Criação das funcionalidades de abrir e fechar modal, selecionar items e inicio da criação de adicionar items no carrinho

--- script.js
+++ script.js
@@ -9,7 +9,38 @@ const carrinhoCont = document.getElementById("carrinho-cont");
 const endereco = document.getElementById("endereco");
 const enderecoErro = document.getElementById("endereco-erro");
 
+//Abrir o modal do carrinho
 carrinhoBtn.addEventListener("click", function(){
     carrinhoModal.style.display = "flex"
 });
 
+//fechar modal quando houver clik fora do modal
+carrinhoModal.addEventListener("click", function(event){
+    if(event.target == carrinhoModal){
+        carrinhoModal.style.display = "none";
+    }
+});
+
+
+fecharBtn.addEventListener("click", function(){
+     carrinhoModal.style.display = "none";
+});
+
+cardapio.addEventListener("click", function(event){
+    //console.log(event.target);
+
+    let parentButton = event.target.closest(".add-carrinho-btn")
+    if(parentButton){
+        const nome = parentButton.getAttribute("data-name");
+        const preco = parseFloat(parentButton.getAttribute("data-price"));
+        
+        //adicionar no carrinho
+        addCarrinho(nome, preco);
+
+    }
+});
+
+//função para adcionar no carrinho 
+function addCarrinho(nome, preco){
+    alert("O item é " + nome)
+}
